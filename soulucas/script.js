cc377fae33a3766b6b216c849f679fe53f127d4f
lucas emanuel <lucascostar098@gmail.com>
2024-01-24 12:20:09 -0300

    update  soulucas

---
+++ soulucas/script.js
@@ -0,0 +1,30 @@
+
+particlesJS("particles-js", {
+        particles: {
+            size:{
+                value: 3,
+            },
+            number: {
+                value:10,
+            }, 
+         line_linked: {
+            enable:false,
+        },
+        color:{
+            value: `#999999`,
+        },
+        // Configurações das partículas
+    },
+    interactivity: {
+        events: {
+            onclick: {
+                enable: false,
+                mode: `push`,
+            },
+        }
+        
+            
+        
+    }
+    // Outras configurações
+});
